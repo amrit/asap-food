593f683ee32be43ad42eef7d42911535bfe37040
Prashan Dharmasena <prashan14@gmail.com>
2015-02-21 17:00:38 -0800

    menu item search working

--- index.js
+++ index.js
@@ -1,13 +1,84 @@
 var express = require('express')
 var app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
-/*app.get('/', function(req, res) {
-	if(req.query.price)
-		res.sendFile('index.html', {'root':__dirname})
-    else
-    	res.send('hello world')
-})*/
+var async = require('async')
+
+var keys = require('./apikeys')
+
+var locu = require('locu')
+var vclient = new locu.VenueClient(keys.locu_api_key)
+var yelp = require("yelp").createClient({
+	consumer_key: keys.yelp_consumer_key,
+	consumer_secret: keys.yelp_consumer_secret,
+	token: keys.yelp_token,
+	token_secret: keys.yelp_token_secret
+})
+
+app.get('/api/search', function(req, res) {
+	yelp.search({term: "food", location: "Berkeley, CA"}, function(error, data) {
+  		//console.log(error);
+  		//console.log(data.businesses);
+  		var return_array = []
+  		var business_names = []
+  		var b = Math.floor(Math.random()*data.businesses.length);
+  		console.log(b)
+  		business = data.businesses[b];
+  		business_names.push(business.name)
+  		
+
+  		vsearch = function(bname, callback){
+  			vclient.search({name: bname, locality: 'Berkeley' }, function(response){
+  					callback(null, response.objects);
+  				});	
+  		}
+
+  		async.map(business_names, vsearch, function(err, results){
+  			if(err){
+  				console.log("error while searching for venues")
+  			} else {
+  				var venue_ids = results.map(function(item){
+  					if(item.length > 0 && item[0].has_menu == true){
+  						return item[0].id
+  					} else {
+  						return null
+  					}
+  				})
+
+  				msearch = function(id, callback){
+  					vclient.get_details([id], function(detail_data){
+  						//console.log(detail_data)
+  						var objects = detail_data.objects || [];
+  						for(obj in objects){
+  							for(m in objects[obj].menus){
+  								menu = objects[obj].menus[m];
+  								for(s in menu.sections){
+  									for(ss in menu.sections[s].subsections){
+  										subsection = menu.sections[s].subsections[ss]
+  										for(c in subsection.contents){
+  											if(subsection.contents[c].price){
+  												console.log(subsection.contents[c])
+  												return_array.push({price: subsection.contents[c].price, name: subsection.contents[c].name})
+  											}
+
+  										}
+  									}
+  								}
+  							}	
+  						}
+  						callback(null, 1)	
+  					})  					
+  				}
+
+  				async.map(venue_ids, msearch, function(err, results){
+  					res.json({restaurant_name: business.name, location: business.location, menu_items: return_array})
+  				})
+
+  			}
+  		})
+
+  		
+	});
+})
 
 app.use(express.static(__dirname))
 
@@ -19,3 +90,25 @@ var server = app.listen(3000, function () {
     console.log('Example app listening at http://%s:%s', host, port)
 
 })
+
+/*
+vclient.get_details([response.objects[0].id], function(detail_data){
+  						var objects = detail_data.objects || [];
+  						for(obj in objects){
+  							for(m in objects[obj].menus){
+  								menu = objects[obj].menus[m];
+  								for(s in menu.sections){
+  									for(ss in menu.sections[s].subsections){
+  										subsection = menu.sections[s].subsections[ss]
+  										for(c in subsection.contents){
+  											if(subsection.contents[c].price){
+  												console.log(subsection.contents[c])
+  												return_array.push({price: subsection.contents[c].price, name: subsection.contents[c].name})
+  											}
+
+  										}
+  									}
+  								}
+  							}	
+  						}
+  					})*/
